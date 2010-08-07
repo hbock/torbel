195f89557fcebb67587f8a875e75f9a8f49b3e72
Harry Bock <hbock@ele.uri.edu>
2010-08-06 23:03:05 -0400

    Rework dnsel implementation to handle query type 3 (me).
    See data-spec and the torel-design.txt document for more
    information.
    
    Handle all queries directly through a DNSServerFactory
    subclass so we have access to information about the requester,
    which is required to handle query type 3.

--- dns.py
+++ dns.py
@@ -15,74 +15,89 @@ from torbel import logger
 from ipaddr import IPAddress
 log = logger.create_logger("tordnsel", logger.DEBUG)
 
-class TorResolver(common.ResolverBase):
-    """ TwistedNames resolver based on torbel.query. """
-    implements(interfaces.IResolver)
+nxdomain = error.DomainError
 
-    def __init__(self, root, filename):
-        common.ResolverBase.__init__(self)
+class TorDNSServerFactory(server.DNSServerFactory):
+    def __init__(self, zone, filename, *args, **kwargs):
+        server.DNSServerFactory.__init__(self, *args, **kwargs)
         self.el = ExitList(filename)
-        self.root = root.split(".")
-        self.root_name = root
+        self.root = zone.split(".")
+        self.root_name = zone
         
-    def _lookup(self, name, cls, type, timeout):
-        def nxdomain(name):
-            return defer.fail(failure.Failure(error.DomainError(name)))
+    def handleQuery(self, message, protocol, address):
+        query = message.queries[0]
+        ip, port = address
 
-        def refused(name):
-            return defer.fail(failure.Failure(error.DNSQueryRefusedError(name)))
+        return self.lookup(ip, query, None).addCallback(
+            self.gotResolverResponse, protocol, message, address
+        ).addErrback(
+            self.gotResolverError, protocol, message, address
+        )
+
+    def exit_search(self, name, dest_ip, dest_port, tor_ip):
+        log.debug("query tor IP %s, dest_ip %s, dest_port %d",
+                  tor_ip, dest_ip, dest_port)
+
+        router = self.el.tor_exit_search(int(tor_ip), dest_ip, dest_port)
+        if router:
+            log.debug("Request for %s:%d matches router %s(%s).",
+                      tor_ip, dest_port, router.idhex, router.nickname)
+            
+            return ([dns.RRHeader(self.root_name, dns.A, dns.IN, 1800,
+                                  payload = dns.Record_A("127.0.0.2"),
+                                  auth = True)],
+                    # Authority section
+                    [dns.RRHeader(self.root_name, dns.NS, dns.IN, 1800,
+                                  payload = dns.Record_NS(self.root_name))],
+                    # Additional section: give the router's idhex and nickname
+                    # as CNAME records.
+                    [dns.RRHeader(self.root_name, dns.CNAME, dns.IN, 1800,
+                                  payload = dns.Record_CNAME("id=" +router.idhex)),
+                     dns.RRHeader(self.root_name, dns.CNAME, dns.IN, 1800,
+                                  payload = dns.Record_CNAME("nickname=" + router.nickname))]
+                    )
+
+        raise nxdomain(name)
+
+    def lookup(self, address, query, timeout):
+        try:
+            return defer.succeed(self._lookup(address, query, timeout))
+        except error.DomainError, e:
+            return defer.fail(failure.Failure(e))
+        
+    def _lookup(self, address, query, timeout):
+        name = str(query.name)
+        type = query.type
+        cls = dns.IN
 
         # Refuse non-IN/A questions.
         if cls != dns.IN or type != dns.A:
             log.debug("We don't handle cls %s type %s", cls, type)
-            return refused(name)
+            return error.DNSQueryRefusedError()
         # Refuse requests for requests that aren't in our zone of authority.
         q = name.split(".")
         if q[-len(self.root):] != self.root:
             log.debug("We are authoritative and don't handle the TLD of %s",
                       repr(name))
-            return refused(name)
+            raise error.DNSQueryRefusedError()
 
         # Break off the root TLD and the query type.
         q = q[:-len(self.root)]
         qtype = q.pop()
 
-        # Query type 1 in torel-design.txt.
+        # DNSEL query type 1 "General IP:Port":
+        # Format: {IP1}.{port}.{IP2}.ip-port.torhosts.example.com
         if qtype == "ip-port":
             # Attempt to parse the DNSEL request.
             try:
                 tor_ip = IPAddress("%s.%s.%s.%s" % (q[3], q[2], q[1], q[0]))
                 dest_port = int(q[4])
                 dest_ip = IPAddress("%s.%s.%s.%s" % (q[8], q[7], q[6], q[5]))
-            # Otherwise barf back NXDOMAIN.
             except:
-                return nxdomain(name)
+                return None
 
-            log.debug("Query type %s, tor IP %s, dest_ip %s, dest_port %d",
-                      qtype, tor_ip, dest_ip, dest_port)
-
-            router = self.el.is_tor_traffic(int(tor_ip), dest_port)
-            if router and router.will_exit_to(int(dest_ip), dest_port):
-                log.debug("Request for %s:%d matches router %s(%s).",
-                          tor_ip, dest_port, router.idhex, router.nickname)
-
-                return defer.succeed((
-                        [dns.RRHeader(self.root_name, dns.A, dns.IN, 1800,
-                                      payload = dns.Record_A("127.0.0.2"),
-                                      auth = True)],
-                        # Authority section
-                        [dns.RRHeader(self.root_name, dns.NS, dns.IN, 1800,
-                                      payload = dns.Record_NS(self.root_name))],
-                        # Additional section: give the router's idhex and nickname
-                        # as CNAME records.
-                        [dns.RRHeader(self.root_name, dns.CNAME, dns.IN, 1800,
-                                      payload = dns.Record_CNAME("id=" +router.idhex)),
-                         dns.RRHeader(self.root_name, dns.CNAME, dns.IN, 1800,
-                                      payload = dns.Record_CNAME("nickname=" + router.nickname))]
-                        ))
-            else:
-                return nxdomain(name)
-
+            return self.exit_search(name, dest_ip, dest_port, tor_ip)
+                
         elif qtype == "ip-port-list":
             dest_port = int(q[0])
             dest_ip = IPAddress("%s.%s.%s.%s" % (q[4], q[3], q[2], q[1]))
@@ -91,23 +106,30 @@ class TorResolver(common.ResolverBase):
 
             addr_list = self.el.will_exit_to(int(dest_ip), dest_port)
             if addr_list:
-                return defer.succeed((
-                        [dns.RRHeader(self.root_name, dns.A, dns.IN, 1800,
+                return ([dns.RRHeader(self.root_name, dns.A, dns.IN, 1800,
                                       payload = dns.Record_A(str(IPAddress(addr))),
                                       auth = True) for addr in addr_list],
                         # Authority section
                         [dns.RRHeader(self.root_name, dns.NS, dns.IN, 1800,
                                       payload = dns.Record_NS(self.root_name))],
                         []
-                        ))
+                        )
             else:
-                return nxdomain(name)
+                raise nxdomain(name)
+            
+        # DNSEL query type 3 "My IP, with port": 
+        # Format: {IP}.{port}.me.torhosts.example.com
+        elif qtype == "me":
+            dest_port = int(q[4])
+            tor_ip  = IPAddress("%s.%s.%s.%s" % (q[3], q[2], q[1], q[0]))
+            return self.exit_search(name, address, dest_port, tor_ip)
+        
         else:
-            return nxdomain(name)
+            raise nxdomain(name)
 
 if __name__ == "__main__":
-    f = server.DNSServerFactory(authorities = [TorResolver("dnsel.torproject.org",
-                                                           "torbel.csv")])
+    f = TorDNSServerFactory(zone = "dnsel.torproject.org", filename = "torbel.csv")
+
     if not "notcp" in sys.argv:
         reactor.listenTCP(53, f)
         log.debug("Listening for TCP queries on port 53.")
