46b260281dd2bff03cdaf3094c7fd6a2c2059fcb
Harry Bock <hbock@ele.uri.edu>
2010-08-14 11:53:43 -0400

    Make check_type global in torbel.utils.
    Useful to dnsel config implementation.

--- utils.py
+++ utils.py
@@ -6,6 +6,15 @@ class ConfigurationError(Exception):
     def __init__(self, message):
         self.message = message
 
+def check_type(config, var, t, func = None, errmsg = None):
+    c = ConfigurationError
+    v = getattr(config, var)
+    if type(v) is not t:
+        raise c("Expected type of %s to be %s, not %s." \
+                        % (var, t, type(v)))
+    if func and not func(v):
+        raise c("%s: %s" % (var, errmsg))
+
 ## TODO: More sanity checks!
 def config_check(config):
     """ Sanity check for TorBEL configuration. """
@@ -26,27 +35,19 @@ def config_check(config):
     if bad_ports:
         raise c("test_port_list: %s are not valid ports." % bad_ports)
 
-    def check_type(var, t, func = None, errmsg = None):
-        v = getattr(config, var)
-        if type(v) is not t:
-            raise c("Expected type of %s to be %s, not %s." \
-                        % (var, t, type(v)))
-        if func and not func(v):
-            raise c("%s: %s" % (var, errmsg))
-
     for var in ["tor_port", "control_port", "stale_router_timeout", "max_built_circuits"]:
-        check_type(var, int, lambda x: x > 0, "Expected positive integer.")
+        check_type(config, var, int, lambda x: x > 0, "Expected positive integer.")
     for var in ["export_gzip", "daemonize", "torctl_debug", "log_syslog", "log_stdout"]:
-        check_type(var, bool)
+        check_type(config, var, bool)
     for var in ["export_file_prefix", "log_file", "torctl_debug_file",
                 "control_password", "tor_host", "test_host", "test_bind_ip"]:
-        check_type(var, str)
+        check_type(config, var, str)
     for var in ["user", "group"]:
         v = getattr(config, var)
         if type(v) is not str and type(v) is not int:
             raise c("%s must be a valid username string or ID number." % var)
 
-    check_type("max_pending_factor", float)
+    check_type(config, "max_pending_factor", float)
 
     # Convert test_scheduler strings to class names.
     try:
@@ -61,7 +62,7 @@ def config_check(config):
                     % (config.test_scheduler, ", ".join(nameToClass.keys())))
     
     for var in ["log_level", "torctl_log_level"]:
-        check_type(var, type(logger.DEBUG),
+        check_type(config, var, type(logger.DEBUG),
                    lambda l: l in (logger.VERBOSE2, logger.VERBOSE1, logger.DEBUG,
                                    logger.INFO, logger.NOTICE,
                                    logger.WARN, logger.ERROR, logger.CRITICAL),
